ae6e1220477628ca80c1bc89711c1c152ba2d5f2
WRIGHT Matthew L <matthew.wright@hexagon.com>
2025-11-09 02:14:27 -0500

    add simple CRUD operations to the reservation service.

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+    private reservations: Reservation[] = [];
+    
+
+
+    //CRUD
+
+    //get all reservations
+    getReservations(): Reservation[] {
+      return this.reservations;
+    }// end getReservations
+
+    // get specific reservation by Id 
+    getReservationById(id: string): Reservation | undefined{    
+      return this.reservations.find(res => res.id == id);
+    }// end getReservationById
+
+    // add a reservation
+    addReservation(reservation: Reservation): void {
+        this.reservations.push(reservation);     
+    }// end addReservation
+
+
+    // delete a reservation 
+    deleteReservation(id: string): void {
+      let index = this.reservations.findIndex(res => res.id === id);      
+        if (index > -1) {
+          this.reservations.splice(index, 1);
+        }     
+    }// end deleteReservation
+
+    updateReservation(updatedReservation: Reservation): void {
+      let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
+      this.reservations[index] = updatedReservation;
+    }// end updateReservation
+    
+
 }
